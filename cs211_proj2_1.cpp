0538a01d79bbff60f75cfcea200f5d33572cdd1b
BlankhDevelopment <42626093+BlankhDevelopment@users.noreply.github.com>
2018-10-26 14:15:56 -0700

    Update cs211_proj2_1.cpp

--- cs211_proj2_1.cpp
+++ cs211_proj2_1.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>                                                                             
 
 #include "cblas.h"                                                                             
-#include <iostream>
+
 #include "lapacke.h"                                                                           
 
 #include <string.h>                                                                            
@@ -13,33 +13,46 @@ using namespace std;
                                                                                                
 
 int main (int argc, const char * argv[]) {                                                     
-  int m = 2;                                                                                   
-  int n = 2;                                                                                   
-  int lda = 2;                                                                                 
-  int ldb = 2;                                                                                 
+
                                                                                                
+
+  int m = 2;                                                                                   
+
+  int n = 2;                                                                                   
+
+  int lda = 2;                                                                                 
+
+  int ldb = 2;                                                                                 
+
+                                                                                               
+
   double * A = new double[m*n];                                                                
+
   double * B = new double[m];                                                                  
+
   int * ipiv = new int[m];                                                                     
+
                                                                                                                                                                                                                                                                                                                                                                                            
 
   A[0] = 4; A[1] = 6;  B[0] = 2;                                                               
+
   A[2] = 3; A[3] = 3;  B[1] = 10;                                                              
 
                                                                                                                                                                                                                                                                                          
 
   LAPACKE_dgetrf( LAPACK_COL_MAJOR, m, n, A, lda, ipiv );                                      
+
                                                                                                
 
   for (int i = 0; i < m; i++) {                                                                
 
-      for (int j = 0; j < n; j++){       
-          cout << " " << A[lda*j+i] << " ";                                
+      for (int j = 0; j < n; j++){                                                             
+
           printf("  %lf ", A[lda*j+i]);                                                        
 
       }                                                                                        
 
-      cout << endl;                                                                      
+      printf("\n");                                                                            
 
                                                                                                
 
@@ -50,9 +63,10 @@ int main (int argc, const char * argv[]) {
   cblas_dtrsm(CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasNonUnit, m, 1, 1.0, A, lda, B, ldb);      
 
 for(int i=0; i < n; i++){
-    
-    cout << " " << B[i] << " ";
-    cout << endl;
+
+     printf("  %lf ", B[i]);
+
+     printf("\n");
 
 }
 
