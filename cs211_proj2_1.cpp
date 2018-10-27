6d78e1b99469c5f586fabbaa2ed6cf68a45d0b77
BlankhDevelopment <42626093+BlankhDevelopment@users.noreply.github.com>
2018-10-27 13:28:48 -0700

    Update cs211_proj2_1.cpp

--- cs211_proj2_1.cpp
+++ cs211_proj2_1.cpp
@@ -29,44 +29,52 @@ int main (int argc, const char * argv[]) {
 
                                                                                                                                                                                                                                                                                                                                                                                            
 
-  A[0] = 4; A[1] = 6;  A[4] =  7;        B[0] = 2;                                                               
-
-  A[2] = 3; A[3] = 3;  A[5] = 4;         B[1] = 10;                                                              
+  A[0] = 4; A[1] = 6;  A[2] =  7;        B[0] = 2;                                                               
+  A[3] = 3; A[4] = 3;  A[5] = 4;         B[1] = 10;                                                              
   A[6] = 6; A[7] = 4;  A[8] = 6;
-                                                                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                                                       
 
   LAPACKE_dgetrf( LAPACK_COL_MAJOR, m, n, A, lda, ipiv );                                      
 
                                                                                                
 
   for (int i = 0; i < m; i++) {                                                                
-
       for (int j = 0; j < n; j++){                                                             
-
           printf("  %lf ", A[lda*j+i]);                                                        
-
       }                                                                                        
-
-      printf("\n");                                                                            
-
-                                                                                               
-
+      printf("\n");                                                                                                                                                                   
   }                                                                                            
+   
+   cout << endl << endl;
 
+  LAPACKE_dtrsm( LAPACK_COL_MAJOR, m, n, A, lda, ipiv );
+
+   for (int i = 0; i < m; i++) {                                                                
+      for (int j = 0; j < n; j++){                                                             
+          printf("  %lf ", A[lda*j+i]);                                                        
+      }                                                                                        
+      printf("\n");                                                                                                                                                                   
+  }       
+
+    cout << endl << endl;
+
+   LAPACKE_dtrsm( LAPACK_COL_MAJOR, m, n, A, lda, ipiv );
+
+   for (int i = 0; i < m; i++) {                                                                
+      for (int j = 0; j < n; j++){                                                             
+          printf("  %lf ", A[lda*j+i]);                                                        
+      }                                                                                        
+      printf("\n");                                                                                                                                                                   
+  }       
+  
                                                                                                
-
   /*cblas_dtrsm(CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasNonUnit, m, 1, 1.0, A, lda, B, ldb);      
-
 for(int i=0; i < n; i++){
-
      printf("  %lf ", B[i]);
-
      printf("\n");
-
 }
 
 */
-
   return 0;
 
 }
