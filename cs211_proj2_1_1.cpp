f664025832b58ec0b9ddbeddf5db771489ffd0ed
BlankhDevelopment <42626093+BlankhDevelopment@users.noreply.github.com>
2018-10-28 16:51:15 -0700

    Update cs211_proj2_1_1.cpp

--- cs211_proj2_1_1.cpp
+++ cs211_proj2_1_1.cpp
@@ -8,18 +8,30 @@
 
 using namespace std;                                                                           
 
+void FillMatrix(double arr1[], double arr2[], int array_size)
+{
+	int m = 0;
+
+	for (m = 0; m < array_size*array_size; ++m)
+	{
+        srand(1);
+		arr1[m] = rand() % 100;
+	}
+
+    for (m = 0; m < array_size; m++)
+    {
+        arr2[m] = rand() % 100;
+    }
+}
                                                                                                
 
 int main (int argc, const char * argv[]) {                                                     
-
+                                                                                            
+  const int n = atoi(argv[1]);
+  int m = n; //# of rows                                                                                                                                                                  
+  int lda = n; //length of first dimenssion                                                                                 
+  int ldb = n;                                                                                 
                                                                                                
-
-  int m = 2; //# of rows                                                                                  
-  int n = 2; //# of columns                                                                                   
-  int lda = 2; //length of first dimenssion                                                                                 
-  int ldb = 2;                                                                                 
-                                                                                               
-
   double *A; 
   double *B;
   double *C;
@@ -28,16 +40,17 @@ int main (int argc, const char * argv[]) {
   A = (double*)malloc(m*n * sizeof(double));
   B = (double*)malloc(m*n * sizeof(double));
   C = (double*)malloc(m*n * sizeof(double));
-  ipiv = (int*)malloc(m* sizeof(int));                                                                     
+  ipiv = (int*)malloc(m* sizeof(int));             
 
-  A[0] = 4; A[1] = 6; A[2] = 3; A[3] = 3;
-/*  A[4] = -2; A[5] = 1; A[6] = -1; A[7] = 0;
+  FillMatrix(A, B, n);                                                        
+
+/*  A[0] = 4; A[1] = 6; A[2] = 3; A[3] = 3;
+  A[4] = -2; A[5] = 1; A[6] = -1; A[7] = 0;
   A[8] = 6; A[9] = 2; A[10] = 1; A[11] = 0;
   A[12] = 0; A[13] = 0; A[14] = 0; A[15] = 0;                                                                                                                                                                                                                                                                                                                                                                                       
-
  B[0] = 4; B[1] = -4; B[2] = 15; B[3] = -1;                                                         
-*/
  B[0] = 2; B[1] = 10;
+*/
 
 cout << endl << "OUTPUTTING MATRIX A: " << endl; 
 
