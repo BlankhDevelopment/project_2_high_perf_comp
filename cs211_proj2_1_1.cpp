3fd6423b4a97ef34e8126ea039c19d4feaa48bd0
BlankhDevelopment <42626093+BlankhDevelopment@users.noreply.github.com>
2018-10-28 14:36:49 -0700

    Update cs211_proj2_1_1.cpp

--- cs211_proj2_1_1.cpp
+++ cs211_proj2_1_1.cpp
@@ -14,10 +14,10 @@ int main (int argc, const char * argv[]) {
 
                                                                                                
 
-  int m = 4; //# of rows                                                                                  
-  int n = 4; //# of columns                                                                                   
-  int lda = 4; //length of first dimenssion                                                                                 
-  int ldb = 4;                                                                                 
+  int m = 2; //# of rows                                                                                  
+  int n = 2; //# of columns                                                                                   
+  int lda = 2; //length of first dimenssion                                                                                 
+  int ldb = 2;                                                                                 
                                                                                                
 
   double *A; 
@@ -30,13 +30,14 @@ int main (int argc, const char * argv[]) {
   C = (double*)malloc(m*n * sizeof(double));
   ipiv = (int*)malloc(m* sizeof(int));                                                                     
 
-  A[0] = 2; A[1] = 0; A[2] = 0; A[3] = 0;
-  A[4] = -2; A[5] = 1; A[6] = -1; A[7] = 0;
+  A[0] = 4; A[1] = 6; A[2] = 3; A[3] = 3;
+/*  A[4] = -2; A[5] = 1; A[6] = -1; A[7] = 0;
   A[8] = 6; A[9] = 2; A[10] = 1; A[11] = 0;
   A[12] = 0; A[13] = 0; A[14] = 0; A[15] = 0;                                                                                                                                                                                                                                                                                                                                                                                       
 
  B[0] = 4; B[1] = -4; B[2] = 15; B[3] = -1;                                                         
-
+*/
+ B[0] = 2; B[1] = 10;
 
 cout << endl << "OUTPUTTING MATRIX A: " << endl; 
 
@@ -49,7 +50,7 @@ cout << endl << "OUTPUTTING MATRIX A: " << endl;
   }                 
   cout << endl << endl;
     
-      LAPACKE_dgetrf( LAPACK_ROW_MAJOR, m, n, A, lda, ipiv );  
+      LAPACKE_dgetrf( LAPACK_COL_MAJOR, m, n, A, lda, ipiv );  
      // cblas_dtrsm(CblasColMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, m, n, 1.0, A, lda, B, ldb);   
 
 cout << endl << "AFTER PERFORMING LU FACTORIZATION ON MATRIX A, WE GET: " << endl;                                                                 
