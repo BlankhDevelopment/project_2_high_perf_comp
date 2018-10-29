129085533379fa26c5781c6e4d9b1fb8e4f5d67b
BlankhDevelopment <42626093+BlankhDevelopment@users.noreply.github.com>
2018-10-28 17:18:09 -0700

    Update cs211_proj2_1_1.cpp

--- cs211_proj2_1_1.cpp
+++ cs211_proj2_1_1.cpp
@@ -38,7 +38,7 @@ int main (int argc, const char * argv[]) {
   int *ipiv;
 
   A = (double*)malloc(m*n * sizeof(double));
-  B = (double*)malloc(m*n * sizeof(double));
+  B = (double*)malloc(m * sizeof(double));
   C = (double*)malloc(m*n * sizeof(double));
   ipiv = (int*)malloc(m* sizeof(int));             
 
