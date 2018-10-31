9984714becbd7237f7cb3eea21eb130c173ab031
BlankHue <42626093+BlankHue@users.noreply.github.com>
2018-10-30 19:05:46 -0700

    Update cs211_proj2.sh

--- cs211_proj2.sh
+++ cs211_proj2.sh
@@ -6,9 +6,5 @@
 #SBATCH -t 00:59:00
 
 export OPENBLAS_NUM_THREADS=1
-./cs211_proj2_1_2 1000
-./cs211_proj2_1_2 2000
-./cs211_proj2_1_2 3000
-./cs211_proj2_1_2 4000
-./cs211_proj2_1_2 5000
+./cs211_proj2_2 3
 
